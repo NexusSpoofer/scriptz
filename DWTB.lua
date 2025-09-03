79224a834096b4553a68c3bbe1772d182f5c229c
NexusSpoofer <kurtiecruz879@gmail.com>
2025-09-03 19:35:54 +0930

    Update DWTB.lua

--- DWTB.lua
+++ DWTB.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/NexusSpoofer/scriptz/refs/heads/main/DWTBV2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NexusSpoofer/scriptz/refs/heads/main/DWTBV2.lua"))()
